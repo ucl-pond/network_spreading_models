6982f1f0d9655166ef43eda6c0aea3773b60e2e0
Ellie Thompson <ethompson9325@gmail.com>
2024-02-02 13:45:08 +0000

    adding weights to the spreading term

--- code/FKPP.py
+++ code/FKPP.py
@@ -6,10 +6,11 @@ from skopt import gp_minimize
 from find_optimal_timepoint import find_optimal_timepoint
 
 class FKPP_class(NDM):
-    def __init__(self, connectome_fname, gamma, t, ref_list, alpha=None, seed_region=None, x0=None, weights=None):
+    def __init__(self, connectome_fname, gamma, t, ref_list, alpha=None, seed_region=None, x0=None, production_weights=None, spreading_weights=None):
         super().__init__(connectome_fname, gamma, t, ref_list, seed_region, x0)
         self.alpha = alpha # logistic growth rate
-        self.weights = weights # weighting for logistic growth term
+        self.production_weights = production_weights # weighting for logistic growth term
+        self.spreading_weights = spreading_weights # weighting for the connectome spread
 
     def logistic_model(self, x):
         return x*(1-x)
@@ -21,8 +22,11 @@ class FKPP_class(NDM):
         '''
 
         #if weights is none use a vector of ones
-        if self.weights is None:
-            self.weights = np.ones(len(self.ref_list))
+        if self.production_weights is None:
+            self.production_weights = np.ones(len(self.ref_list))
+
+        if self.spreading_weights is None:
+            self.spreading_weights = np.ones(len(self.ref_list))
 
         H = self.get_Laplacian()
 
@@ -33,7 +37,9 @@ class FKPP_class(NDM):
         x_t[:,0] = self.get_initial_conditions() # set first time point to initial conditions.
 
         for kt in range(1,self.Nt):  #iterate through time points, calculating the node atrophy as you go along
-            x_t[:,kt] = x_t[:,kt-1] + self.alpha*self.NDM_dx(H,x_t[:,kt-1]) + (1-self.alpha)*self.logistic_model(x_t[:,kt-1])*self.weights*self.dt
+            x_t[:,kt] = x_t[:,kt-1] \
+                + self.alpha*self.spreading_weights*self.NDM_dx(H,x_t[:,kt-1]) \
+                + (1-self.alpha)*self.logistic_model(x_t[:,kt-1])*self.production_weights*self.dt
 
         return x_t
     
@@ -54,15 +60,16 @@ class FKPP_class(NDM):
                 ref_list=self.ref_list,
                 alpha=params["alpha"],
                 seed_region=params["seed_region"],
-                weights=self.weights
+                production_weights=self.production_weights,
+                spreading_weights=self.spreading_weights
             )
             model_output = fkpp.run_FKPP()
             min_idx, prediction, SSE = find_optimal_timepoint(model_output, target_data)
             return SSE
 
-        res = gp_minimize(objective, dimensions=space, 
-                        acq_func="gp_hedge", 
-                        n_calls=n_calls, 
+        res = gp_minimize(objective, dimensions=space,
+                        acq_func="gp_hedge",
+                        n_calls=n_calls,
                         n_initial_points=n_initial_points,
                         random_state=42,
                         initial_point_generator="sobol"
